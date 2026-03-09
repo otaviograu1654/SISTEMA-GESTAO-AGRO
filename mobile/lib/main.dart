3f600e22225158c63926c170ea081b849f60e971
tshgost <gabrielunion111@gmail.com>
2026-03-09 17:50:03 -0300

    Adicionando a classe PecuariaApp

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -4,3 +4,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const PecuariaApp());
 }
+
+class PecuariaApp extends StatelessWidget {
+  const PecuariaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SGA Pecuária',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SGA Pecuária'),
+        ),
+        body: const Center(
+          child: Text('App iniciado com sucesso'),
+        ),
+      ),
+    );
+  }
+}
