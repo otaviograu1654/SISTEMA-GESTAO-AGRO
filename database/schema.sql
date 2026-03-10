1cf3d80835c408c0a6ebe69b0740d2579a7c1937
otavio1654 <ofernandescaetano@unipam.edu.br>
2026-03-10 19:53:19 -0300

    Add ciclo_reprodutivo table to schema
    (teste)  adicionando uma nova tabela de ciclo reprodutivo no sql

--- database/schema.sql
+++ database/schema.sql
@@ -46,3 +46,17 @@ CREATE TABLE IF NOT EXISTS financeiro (
     data_lancamento DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS `ciclo_reprodutivo` (
+  `id_ciclo` int(11) NOT NULL AUTO_INCREMENT,
+  `id_matriz` int(11) DEFAULT NULL,
+  `id_touro_ou_semen` int(11) DEFAULT NULL,
+  `data_inseminacao` date DEFAULT NULL,
+  `confirmacao_prenhez` tinyint(1) DEFAULT 0,
+  `previsao_parto` date DEFAULT NULL,
+  PRIMARY KEY (`id_ciclo`),
+  KEY `id_matriz` (`id_matriz`),
+  KEY `id_touro_ou_semen` (`id_touro_ou_semen`),
+  CONSTRAINT `1` FOREIGN KEY (`id_matriz`) REFERENCES `animal` (`id_animal`),
+  CONSTRAINT `2` FOREIGN KEY (`id_touro_ou_semen`) REFERENCES `animal` (`id_animal`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
