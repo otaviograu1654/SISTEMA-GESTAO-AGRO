29e0eef1563ef58bb08b21d200f322caa7d3b058
tshgost <gabrielunion111@gmail.com>
2026-03-10 23:42:56 -0300

    Call popularSelectAnimais after updating cards

--- backend/index.php
+++ backend/index.php
@@ -442,6 +442,7 @@
                 });
 
                 atualizarCards(animais);
+                popularSelectAnimais(animais);
 
             } catch (erro) {
                 loading.textContent = 'Erro ao carregar animais.';
